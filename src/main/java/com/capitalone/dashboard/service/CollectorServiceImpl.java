c47424e7ab77eba53527d62c71c2168b680a3a9d
peidong hu <zhouyibhic@gmail.com>
2021-01-02 21:44:36 -0600

    change Deploy to Service

--- src/main/java/com/capitalone/dashboard/service/CollectorServiceImpl.java
+++ src/main/java/com/capitalone/dashboard/service/CollectorServiceImpl.java
@@ -104,7 +104,7 @@ public class CollectorServiceImpl implements CollectorService {
 
     public static List makeCollectionItmes(){
         Collector collector = makeCollector("MockDeployCollector", CollectorType.Deployment);
-        CollectorItem item1 = makeCollectorItem(collector, "Deploy 1", false);
+        CollectorItem item1 = makeCollectorItem(collector, "Service 1", false);
 //        CollectorItem item2 = makeCollectorItem(collector, "Deploy 2", true);
         List cis = Arrays.asList(item1);//, item2);
 
